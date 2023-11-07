9282e399d62632ac4a7d5354ac671749968ac3e5
Deba Lakshmi B <64346248+debalakshmi@users.noreply.github.com>
2023-11-07 22:49:46 +0530

    Add files via upload

--- scapper.py
+++ scapper.py
@@ -31,7 +31,7 @@ def get_product_link(query):
     try:
         # The search() function returns a generator, so use next() to get the first result
         product_url = next(search(query, num=1, stop=1))
-        print(product_url)
+        # print(product_url)
         return product_url
     except Exception as e:
         print("No result found", e)
@@ -54,59 +54,73 @@ def get_productName(soup):
 def get_ean_price(soup, url):
     if "getic" in url:
         ean_number = soup.find('div', class_='section-cell product-ean order-4')
-        ean_number = ean_number.find('div', {'class': 'cell-content-value'}).text
+        ean_number_1 = ean_number.find('div', {'class': 'cell-content-value'}).text
+        print(ean_number_1)
 
         # currency = soup.find_all('span', class_="product-price-currency")[0]
         # currency_symbol = currency.get_text().strip()
         # amount = soup.find_all('span', class_="product-price-value")[0]
         # amount_value = amount.get_text().strip()
-        # price = currency_symbol + ' ' + amount_value
-        price= "$ 150"
-        return ean_number, price
-    elif "microless" in url:
-        ean_number = soup.find_all('div', class_='section-cell product-ean order-4')
-        ean_number = ean_number.find('div', {'class': 'cell-content-value'}).text
-        return "to do"
+        # # price = currency_symbol + ' ' + amount_value
+        # price= "$ 150"
+        return ean_number_1
+    # elif "microless" in url:
+    #     ean_number = soup.find_all('div', class_='section-cell product-ean order-4')
+    #     ean_number = ean_number.find('div', {'class': 'cell-content-value'}).text
+    #     return "to do"
     # elif "hubx"
 
 
 if __name__ == "__main__":
 
     urls = [
-        "https://getic.com",
+        "https://www.getic.com/product/",
         # "https://uae.geticmicroless.com",
         # "https://hubx.com",
         # "https://texub.com",
         # "https://gear-up.me"
     ]
-    df = pd.read_csv("product.csv")
-    df_list = []
+    df = pd.read_excel("./TopSKU.xlsx")
+    print(df)
     for url in urls:
         extract_df = {"EAN/BARCODE": [], "PRICE WITH CURRENCY": [], "PRODUCT_LINK": []}
         for product in df['SKU']:
             # query = f'USW-Pro-24-POE site:https://www.getic.com/'
-            query = f'{product} site:{url}'
+            query = f'{product} {url}'
+            print(query)
             product_url = get_product_link(query)
+            print("product:" + product_url)
             if product_url:
                 page = requests.get(product_url)
                 soup = BeautifulSoup(page.content, "html.parser")
-                ean, price = get_ean_price(soup, url)
-                extract_df['EAN/BARCODE'].append(ean)
-                extract_df['PRICE WITH CURRENCY'].append(price)
-                extract_df['PRODUCT_LINK'].append(product_url)
-            else:
-                extract_df['EAN/BARCODE'].append('N/A')
-                extract_df['PRICE WITH CURRENCY'].append('N/A')
-                extract_df['PRODUCT_LINK'].append('N/A')
+                ean = get_ean_price(soup, url)
+            #     extract_df['EAN/BARCODE'].append(ean)
+            #     extract_df['PRICE WITH CURRENCY'].append(price)
+            #     extract_df['PRODUCT_LINK'].append(product_url)
+            # else:
+            #     for product1 in df['ITEM NAME']:
+            #         # query = f'USW-Pro-24-POE site:https://www.getic.com/'
+            #         query = f'{product1} {url}'
+            #         print(query)
+            #         product_url = get_product_link(query)
+            #         print("product:" + product_url)
+            #         page = requests.get(product_url)
+            #         soup = BeautifulSoup(page.content, "html.parser")
+            #         ean = get_ean_price(soup, url)
+
+            # else:
+            #     extract_df['EAN/BARCODE'].append('N/A')
+            #     extract_df['PRICE WITH CURRENCY'].append('N/A')
+            #     extract_df['PRODUCT_LINK'].append('N/A')
 
         # globals()[site] = pd.concat([df, pd.DataFrame.from_dict(extract_df)], axis=1)
-        df_list.append(pd.concat([df, pd.DataFrame.from_dict(extract_df)]))
+        # df_list.append(pd.concat([df, pd.DataFrame.from_dict(extract_df)]))
 
-    with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer:
-        for df, sheet_name in zip(df_list, urls):
-            url = url.replace('-', '_')
-            site = url.split('/')[2]
-            df.to_excel(writer, sheet_name=site, index=False)
+    # with pd.ExcelWriter('C:/Users/LENOVO/Downloads/TLM/output.xlsx', engine='openpyxl') as writer:
+    #     for df, sheet_name in zip(df_list, urls):
+    #         url = url.replace('-', '_')
+    #         site = url.split('/')[2]
+    #         df.to_excel(writer, sheet_name=site, index=False)
 
     # with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer:
     #     df1.to_excel(writer, sheet_name='Sheet1', index=False)
